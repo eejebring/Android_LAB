bf00c7cca83a30305f19a381b512ce243a5efcc8
eejebring <dev@ejebring.com>
2024-10-30 15:49:22 +0100

    Some List styling

--- app/src/main/java/com/example/android_lab/MainActivity.kt
+++ app/src/main/java/com/example/android_lab/MainActivity.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import com.example.android_lab.ui.theme.Android_LABTheme
 
@@ -26,14 +34,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Android_LABTheme {
-                val notes = remember { mutableListOf(Note("Do this", Category.Task, "yeah all of this"), Note("Do that", Category.Task, "yeah all of that")) }
+                MainView()
+            }
+        }
+    }
+}
 
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    NotePreviewList(notes)
+@Composable
+fun MainView() {
+    val notes = remember { mutableListOf(Note("Do this", Category.Task, "yeah all of this"), Note("Do that", Category.Task, "yeah all of that")) }
+
+    Scaffold (modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text("Total amount of notes: " + notes.count())
+            LazyColumn() {
+                items(notes) { note ->
+                    NotePreview(note)
                 }
             }
         }
@@ -41,34 +57,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun NotePreviewList(notes: List<Note>) {
-    LazyColumn( modifier = Modifier.padding(top = Dp(100F))) {
-        items(notes) { note ->
-            NotePreview(note)
-        }
-    }
-}
-
-@Composable
 fun NotePreview(note: Note) {
-    Row {
-        Text(note.title)
+    //HorizontalDivider()
+    Column ( modifier = Modifier
+        .padding(Dp(2F))
+        .background(color = Color.LightGray, shape = RoundedCornerShape(10))
+        .fillMaxWidth()
+        .padding(Dp(2F))
+    ) {
+        Text(note.title, fontWeight = FontWeight.Bold)
         Text(note.description)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Android_LABTheme {
-        Greeting("Android")
     }
 }
