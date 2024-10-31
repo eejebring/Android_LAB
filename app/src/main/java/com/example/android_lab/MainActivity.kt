cee3d67433eeb813fd35d3d357c01755627cae68
eejebring <dev@ejebring.com>
2024-10-31 16:04:56 +0100

    dark theme

--- app/src/main/java/com/example/android_lab/MainActivity.kt
+++ app/src/main/java/com/example/android_lab/MainActivity.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,8 +18,6 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import com.example.android_lab.ui.theme.Android_LABTheme
 import com.example.android_lab.ui.theme.LargeFontSize
@@ -33,7 +28,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Android_LABTheme {
+            Android_LABTheme (darkTheme = true) {
                 MainView()
             }
         }
--- app/src/main/java/com/example/android_lab/NotePreview.kt
+++ app/src/main/java/com/example/android_lab/NotePreview.kt
@@ -6,12 +6,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 
@@ -21,7 +24,7 @@ fun NotePreview(note: Note, index: Int) {
     //HorizontalDivider()
     Column ( modifier = Modifier
         .padding(Dp(2F))
-        .background(color = Color.LightGray, shape = RoundedCornerShape(10))
+        .background(color = MaterialTheme.colorScheme.primaryContainer, shape = RoundedCornerShape(10))
         .fillMaxWidth()
         .padding(Dp(2F))
     ) {
@@ -30,6 +33,7 @@ fun NotePreview(note: Note, index: Int) {
             Text(note.description)
             Button(onClick = {}, modifier = Modifier.align(Alignment.CenterEnd)) {
                 Text("" + index)
+                Icon(Icons.Filled.Edit, contentDescription = "Edit note")
             }
         }
     }
