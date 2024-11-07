fa650923451772dd821c51c9f01198808386791f
eejebring <dev@ejebring.com>
2024-11-07 18:02:00 +0100

    Delete button on edit

--- app/src/main/java/com/example/android_lab/EditNote.kt
+++ app/src/main/java/com/example/android_lab/EditNote.kt
@@ -3,12 +3,14 @@ package com.example.android_lab
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
 import com.example.android_lab.ui.theme.LargeFontSize
@@ -22,13 +24,30 @@ fun EditNoteView(noteList: MutableList<Note>, editingNote: Int, navController: N
     val newDescription = remember { mutableStateOf(note.description) }
 
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Editing Note", fontWeight = FontWeight.Bold, fontSize = LargeFontSize, modifier = Modifier.align(Alignment.TopCenter))
+        Text(
+            "Editing Note",
+            fontWeight = FontWeight.Bold,
+            fontSize = LargeFontSize,
+            modifier = Modifier.align(Alignment.TopCenter)
+        )
+
+        Button(
+            onClick = {
+                noteList.removeAt(editingNote)
+                navController.navigate("main")
+            },
+            modifier = Modifier.align(Alignment.TopEnd),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+        ) {
+            Text("Delete", fontSize = LargeFontSize)
+        }
 
         NoteForm(newTitle, newCategory, newDescription)
 
         Button(
             onClick = {
-                noteList[editingNote] = Note(newTitle.value, newCategory.value, newDescription.value)
+                noteList[editingNote] =
+                    Note(newTitle.value, newCategory.value, newDescription.value)
                 navController.navigate("main")
             },
             modifier = Modifier.align(Alignment.BottomStart),
